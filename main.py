939bc2681fce4a5e38a1653aefbf73d2df790420
BaderAhmedd <rhoubywyse@gmail.com>
2026-03-18 04:01:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,19 @@
 import telebot
-import requests
-import time
 from threading import Thread
 from flask import Flask
 
-# التوكن الخاص بك
 TOKEN = '8743111071:AAH4kh9suBgGR9YG3ZTz8a9_v74M-T9qP4U'
 bot = telebot.TeleBot(TOKEN)
-monitored = {}
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "🚀 البوت يعمل الآن بنجاح على السيرفر 24/7")
-
-@bot.message_handler(commands=['add'])
-def add(message):
-    try:
-        user = message.text.split()[1].replace('@', '')
-        monitored[user] = message.chat.id
-        bot.reply_to(message, f"✅ بدأ رصد الحساب: @{user}")
-    except:
-        bot.reply_to(message, "❌ خطأ! استخدم الأمر كذا: /add user")
+    bot.reply_to(message, "🚀 أبشرك يا بدر.. البوت شغال الحين 24 ساعة!")
 
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is alive!"
+    return "Bot is Alive!"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
@@ -37,5 +24,4 @@ def keep_alive():
 
 if name == "__main__":
     keep_alive()
-    print("Bot is starting...")
     bot.infinity_polling()
